e227655b704af57f8c4b224795cb3b49074fb37b
yootaeyun <ew20@naver.com>
2022-06-28 23:45:58 +0900

    ListProducts (상품 리스트 추가)

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 # Lessons
 
-1. Introduction (소개)
-2. Install Tools (도구 설치)
-3. Create React App (React앱 만들기)
-4. Create Git Repository (Git 리포지토리 생성)
+5. ListProducts
+   1. Introduction (소개)
+   2. Install Tools (도구 설치)
+   3. Create React App (React앱 만들기)
+   4. Create Git Repository (Git 리포지토리 생성)
--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import data from "./Data";
+
 function App() {
   return (
     <div>
@@ -5,7 +7,28 @@ function App() {
         <a href="/">Amazon</a>
       </header>
 
-      <main>상품 리스트</main>
+      <main>
+        <h1>인기 제품</h1>
+
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                  <p>
+                    <strong>{product.price}</strong>
+                  </p>
+                </a>
+                <button>장바구니 추가</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
